df87d996a922ee91a1f516cbf3291f0b9443346e
tevetely <cvetygetova@yahoo.com>
2018-09-04 15:14:59 +0300

    Movie
    Added models

---
+++ MoviesCatalogueApplication/app/src/main/java/com/quicksilver/moviesapp/models/Movie.java
@@ -0,0 +1,23 @@
+package com.quicksilver.moviesapp.models;
+
+import java.util.List;
+
+public class Movie {
+    public int id;
+    public String name;
+    public String imageUrl;
+    public String cast;
+    public String description;
+
+    public Movie() {
+        // public constructor is needed for parsing from/to JSON to work
+    }
+
+    public Movie(int id, String name, String imageUrl, String cast, String description) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.cast = cast;
+        this.description = description;
+    }
+}
--- MoviesCatalogueApplication/app/src/test/java/com/quicksilver/moviesapp/ExampleUnitTest.java
+++ MoviesCatalogueApplication/app/src/test/java/com/quicksilver/moviesapp/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.quicksilver.user.moviescatalogueapplication;
+package com.quicksilver.moviesapp;
 
 import org.junit.Test;
 
