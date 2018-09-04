04363cbffd4e0ef62b4131ad610697cad49f87fc
tevetely <cvetygetova@yahoo.com>
2018-09-04 15:56:42 +0300

    OkHttpHttpRequester
    Added HttpRequester

---
+++ MoviesCatalogueApplication/app/src/main/java/com/quicksilver/moviesapp/Constants.java
@@ -0,0 +1,5 @@
+package com.quicksilver.moviesapp;
+
+public class Constants {
+    public static final String BASE_SERVER_URL = "http://192.168.1.1:8080/api";
+}
---
+++ MoviesCatalogueApplication/app/src/main/java/com/quicksilver/moviesapp/http/HttpRequester.java
@@ -0,0 +1,9 @@
+package com.quicksilver.moviesapp.http;
+
+import java.io.IOException;
+
+public interface HttpRequester {
+    String get(String url) throws IOException;
+
+    String post(String url, String body) throws IOException;
+}
